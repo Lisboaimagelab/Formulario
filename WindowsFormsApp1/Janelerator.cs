9c105e4c51b2bef2f882d642fe182be8a891b64e
Vladyslav Rysich <lisboaimagelab@gmail.com>
2018-12-22 19:12:34 +0000

    fun;\ao

--- WindowsFormsApp1/Janelerator.cs
+++ WindowsFormsApp1/Janelerator.cs
@@ -13,6 +13,8 @@ namespace WindowsFormsApp1
     public partial class Janelerator : Form
     {
         int z = 1000;
+        bool movimentoNeg = false;
+        int velocidade = 100;
 
         public Janelerator()
         {
@@ -104,6 +106,7 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
+            movimentoNeg = false;
             timer1.Enabled = true;
             motorX.Value = 0;
         }
@@ -176,25 +179,46 @@ namespace WindowsFormsApp1
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (z >= 0)
+            if (z >= 0 && movimentoNeg == false && checkBoxSensorHomeX.Checked == false) // movimento negativo rapido 
             {
-                if (checkBoxSensorHomeX.Checked == false)
+                motorX.Value = z;
+                textBox1.Text = z.ToString();
+                z--;
+            }
+            if (z >= 0 && movimentoNeg == false && checkBoxSensorHomeX.Checked == true && velocidade != 0) // movimento negativo lento
+            {
+                int n = 50; // valor de intervalo
+                timer1.Interval = n; 
+                motorX.Value = z;
+                textBox1.Text = z.ToString();
+                z--;
+                n++;
+                velocidade--;
+
+                if(velocidade == 0)
                 {
-                    motorX.Value = z;
-                    textBox1.Text = z.ToString();
-                    z--;
-                }
-                else
-                {
-                    z++;
+                    movimentoNeg = true;
                 }
             }
-            else
+            if (z >= 0 && movimentoNeg == true && checkBoxSensorHomeX.Checked == true && velocidade == 0) //
+            {
+                timer1.Interval = 100;
+                motorX.Value = z;
+                textBox1.Text = z.ToString();
+                z++;
+            }
+            if (z >= 0 && movimentoNeg == true && checkBoxSensorHomeX.Checked == false) //
             {
                 timer1.Stop();
                 z = 0;
             }
-
+            /*           else
+                        {
+                            timer1.Stop();
+                            z = 0;
+                            movimentoNeg = true;
+                        }
+            */
 
         }
 
